35cbed7cfb897f14c11f7db029e925d76e7dbb9b
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 19:19:44 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan10/logika/PBO2PERT110118046Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert1.pkg10118046.latihan10.logika;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan relasi operator logika dengan menggunakan tipe data boolean
+ */
+public class PBO2PERT110118046Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur>= 17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+
+}
